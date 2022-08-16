95d31ce9cf0fc7d4fd46934774e9c6bd300fe212
NickHafner <nickhafner@outlook.com>
2022-08-16 18:30:18 -0500

    dependency updated

---
+++ lib/supabase.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import "dotenv";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  localStorage: AsyncStorage as any,
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: false,
+});
