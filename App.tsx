11018ba7fcb1a6aadaeac03ec1d0d0c0a755dd47
NickHafner <nickhafner@outlook.com>
2022-09-04 07:56:38 -0500

    env retrieval updated

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import { ThemeProvider } from '@shopify/restyle';
 import theme from './styles/theme';
 import darkTheme from './styles/darkTheme';
 import Box from './styles/Box';
-import Text from './styles/Text';
-import { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { Button } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import LoginPage from './src/pages/Login';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 0 } },
@@ -54,10 +54,10 @@ export default function App() {
           justifyContent="center"
           paddingHorizontal="xs"
           onLayout={onLayoutRootView}>
-          <Text>Open up App.tsx to start working on your app!</Text>
+          <LoginPage />
           <Button
             onPress={() => setThemeMode(themeMode === 'dark' ? 'light' : 'dark')}
-            title="Button"
+            title="Theme"
           />
         </Box>
       </QueryClientProvider>
